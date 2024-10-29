5dced048bf6dacaff037f1af6126bc2edb93c5e3
EC2 Default User <ec2-user@ip-172-31-18-206.ec2.internal>
2024-10-29 01:33:20 +0000

    changed instance namedadadadw

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "amazon-linux-instance" {
+/*resource "aws_instance" "amazon-linux-instance" {
   count = 1
   ami = "ami-0583d8c7a9c35822c"
   instance_type = "t2.medium"
@@ -11,7 +11,7 @@ resource "aws_instance" "amazon-linux-instance" {
   tags = {
     Name = "jenkins-deployment-server-2" 
   }
-}
+} */
 
 resource "aws_security_group" "default" {
   name = "allow http-ssh-"
